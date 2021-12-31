9de5326b47cb37b090d3192cbabea4188c8271d0
Brian Preston <brian8181@gmail.com>
2021-12-31 08:07:34 -0600

    started adding mutli params for create classes

--- class_tmpl/run.create.class.sh
+++ class_tmpl/run.create.class.sh
@@ -2,4 +2,7 @@ CLASS_NAME=$1
 INPUT_PATH=~/bin
 OUTPUT_PATH=.
 
-${INPUT_PATH}/create.class.sh ${CLASS_NAME} ${INPUT_PATH} ${OUTPUT_PATH}
+for i in "$@"; do
+	${CLASS_NAME}=$i
+	${INPUT_PATH}/create.class.sh ${CLASS_NAME} ${INPUT_PATH} ${OUTPUT_PATH}
+done
--- run.create.class.sh
+++ run.create.class.sh
@@ -1,5 +1,10 @@
-CLASS_NAME=$1
 INPUT_PATH=~/bin
 OUTPUT_PATH=.
 
-${INPUT_PATH}/create.class.sh ${CLASS_NAME} ${INPUT_PATH} ${OUTPUT_PATH}
+for i in "$@"; do
+
+echo $i
+CLASS_NAME=$i
+${INPUT_PATH}/create.class.sh ${CLASS_NAME}
+
+done
