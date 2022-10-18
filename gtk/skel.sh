9de48e83dd4ec43988db5ab156e0eef611e97f65
Brian K Preston <brian8181@gmail.com>
2022-10-18 05:59:41 -0500

    add skel.sh to gtk project, minor update to minimal

--- gtk/skel.sh
+++ gtk/skel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 FILE='skel.sh'
-VERSION='0.1'
-FILE_DATE='October 30, 2021'
+VERSION='0.1.2'
+FILE_DATE='June 7, 2022'
 AUTHOR='Brian K Preston'
 EMAIL='brian8181@gmail.com'
 WWW='https://github.com/brian8181'
@@ -45,7 +45,7 @@ PROJECT_PATH=$3/$APP_NAME
 mkdir -p $PROJECT_PATH
 cp -rf $TEMPLATE_PATH/* $PROJECT_PATH 
 touch $PROJECT_PATH/.project # create file that marks this a project folder
-#cp -rf $TEMPLATE_PATH/.project $PROJECT_PATH 
+#cp -rf $TEMPLATE_PATH/.project $PROJECT_PATH
 
 pushd $PROJECT_PATH
 cat ./configure.ac.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > configure.ac
@@ -54,36 +54,52 @@ chmod 644 AUTHORS ChangeLog NEWS README* configure.ac Makefile.am
 popd
 
 pushd $PROJECT_PATH/build
-cat ./Makefile.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > Makefile
-#rm Makefile.tmpl
+cat ./Makefile.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > Makefile.tmp
+rm Makefile.tmpl
+mv Makefile.tmp Makefile.tmpl
+cat Makefile.tmpl | sed "s/@@CLASS_NAME@@//g" > Makefile
+#rm *.tmpl
 chmod 644 Makefile 
 popd
 
+# get dir name, aka template name
+# TEMPLATE_NAME=${TEMPLATE_PATH##/*/}
+# if[ ${TEMPLATE_NAME} == "basic" ]; then
+# fi
+
+# if [ -d "$PROJECT_PATH/man" ]
+# then
+# fi
+
 pushd $PROJECT_PATH/src
 cat  ./app.cpp.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > ${APP_NAME}.cpp
-# cat  ./app.hpp.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > ${APP_NAME}.hpp
+rm ./app.cpp.tmpl 
+cat  ./app.hpp.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > ${APP_NAME}.hpp
 cat  ./Makefile.am.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > Makefile.am
-# cat  ./app_test.cpp.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}_test/g" > ${APP_NAME}_test.cpp
-# cat  ./@app@_test.hpp.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}_test/g" > ${APP_NAME}_test.hpp
-chmod 644 *.cpp *.hpp Makefile*
+cat  ./app_test.cpp.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}_test/g" > ${APP_NAME}_test.cpp
+cat  ./@app@_test.hpp.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}_test/g" > ${APP_NAME}_test.hpp
 rm *.tmpl
+chmod 644 *.cpp *.hpp Makefile*
 popd
 
-# cd $PROJECT_PATH/man
-# cat  ./app.1.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > ${APP_NAME}.1
-# cat  ./install.sh.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > install.sh
-# cat  ./Makefile.am.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > Makefile.am
-# rm *.tmpl
-# chmod 644 install.sh Makefile* ${APP_NAME}.1
+pushd $PROJECT_PATH/man
+cat  ./app.1.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > ${APP_NAME}.1
+cat  ./install.sh.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > install.sh
+cat  ./Makefile.am.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > Makefile.am
+rm *.tmpl
+chmod 644 install.sh Makefile* ${APP_NAME}.1
+popd
 
+# move to project root, and stay
 pushd $PROJECT_PATH
 mv gitignore_template .gitignore
 rm *skel.sh
-
 git init
 git add '*'
 git commit -m 'initial commit'
 popd
+# add to git hub
+# gh --repo @@APP_NAME@@
 
 PRINT_DEBUG "$FILE -> Exiting.   @ $DATE"
 
