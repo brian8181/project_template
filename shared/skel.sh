f0a2af9e9358273a2de96941f4806d6c4f052648
Brian K Preston <brian8181@gmail.com>
2023-02-28 15:49:02 -0600

    move make file to root

--- shared/skel.sh
+++ shared/skel.sh
@@ -43,7 +43,6 @@ function PRINT_INFO
 }
 
 PRINT_INFO "$FILE -> Running... @ $DATE"
-
 ##{ BEGIN YOUR CODE  }##
 
 APP_NAME=$1 
@@ -56,50 +55,41 @@ PROJECT_PATH=$(pwd)/$APP_NAME
 mkdir -p $PROJECT_PATH
 cp -rf $TEMPLATE_PATH/* $PROJECT_PATH/
 touch $PROJECT_PATH/.project # create file that marks this a project folder
-pushd $PROJECT_PATH
+pushd $PROJECT_PATH > /dev/null
 
 # do makefile
-cat ./build/Makefile.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > Makefile.tmpl
-cat Makefile.tmpl | sed "s/@@CLASS_NAME@@//g" > Makefile # delete ? 
-rm ./build/*
+cat ./Makefile.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > Makefile.tmp
+cat Makefile.tmp | sed "s/@@CLASS_NAME@@//g" > Makefile
+rm Makefile.tmp*
 chmod 644 Makefile 
 
 if [[ ${TEMPLATE_PATH##/*/} = "basic" || ${TEMPLATE_PATH##/*/} = "gtk" ]]; then
 
 	# do auto tools files
-	cat ./configure.ac.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > configure.ac
-	rm configure.ac.tmpl
-	chmod 644 AUTHORS ChangeLog NEWS README* configure.ac Makefile.am
+	# cat ./configure.ac.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > configure.ac
+	# rm configure.ac.tmpl
+	# chmod 644 AUTHORS ChangeLog NEWS README* configure.ac Makefile.am
 
-	pushd ./src
+	pushd ./src > /dev/null
 	cat  ./app.cpp.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > ${APP_NAME}.cpp
-	rm ./app.cpp.tmpl 
 	cat  ./app.hpp.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > ${APP_NAME}.hpp
 	cat  ./Makefile.am.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > Makefile.am
 	rm *.tmpl
 	chmod 644 *.cpp *.hpp Makefile*
-	popd
+	popd > /dev/null
 
-	# pushd $PROJECT_PATH/man
-	# cat  ./app.1.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > ${APP_NAME}.1
-	# cat  ./install.sh.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > install.sh
-	# cat  ./Makefile.am.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > Makefile.am
-	# rm *.tmpl
-	# chmod 644 install.sh Makefile* ${APP_NAME}.1
-	#popd
-else
-	echo PRINT_DEBUG "Unknown Type"
+	pushd ./man > /dev/null
+	cat  ./app.1.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > ${APP_NAME}.1
+	cat  ./install.sh.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > install.sh
+	cat  ./Makefile.am.tmpl | sed "s/@@APP_NAME@@/${APP_NAME}/g" > Makefile.am
+	rm *.tmpl
+	chmod 644 install.sh Makefile* ${APP_NAME}.1
+	popd > /dev/null
 fi
 
 mv gitignore_template .gitignore
-popd
-# no git for now!
-# git init repository
-# git init
-# git add '*'
-# git commit -m 'initial commit'
+popd > /dev/null
 
 ##{ END YOUR CODE  }##
-
 PRINT_INFO "$FILE -> Exiting.   @ $DATE"
 
