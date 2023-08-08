48d85afb49dd832b535c6d9037fe89ecf5b3c5a4
Brian K Preston <brian8181@gmail.com>
2023-08-08 08:44:46 -0500

    added header.snip ...

--- shared/name_val_replace.sh
+++ shared/name_val_replace.sh
@@ -36,7 +36,6 @@ cat ${INPUT_PATH:-"/home/$USER_NAME/bin"}/class.base.hpp.tmpl \
 #./src/${CLASS_NAME}.hpp
 #> ${OUTPUT_PATH:-"."}/src/${CLASS_NAME}.hpp.tmpl
 
-
 cat ${INPUT_PATH:-"/home/$USER_NAME/bin"}/class.base.cpp.tmpl \
 | sed "s/@@CLASS_NAME@@/${CLASS_NAME}/g" \
 | sed "s/@@AUTHOR@@/${AUTHOR}/g" \
@@ -45,6 +44,5 @@ cat ${INPUT_PATH:-"/home/$USER_NAME/bin"}/class.base.cpp.tmpl \
 | sed "s/@@BUILD_DATE@@/${BUILD_DATE}/g" \
 | sed "s/@@FILE_NAME@@/${CLASS_NAME}.cpp/g" \
 > ${OUTPUT_PATH:-"."}/src/${CLASS_NAME}.cpp.tmpl	
-
 
 # now replace base class tag
