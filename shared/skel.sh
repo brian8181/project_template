3b68789db4eedf6831f226fb752c7270a753109d
Brian K Preston <brian8181@gmail.com>
2023-08-27 00:12:15 -0500

    working on headers ....

--- shared/skel.sh
+++ shared/skel.sh
@@ -46,12 +46,12 @@ function ADD_HEADERS
 {
 	FILE=$1
 	cat  "./${FILE}" \
-	| sed "s/@@APP_NAME@@/${APP_NAME}/g" \
-	| sed "s/@@AUTHOR@@/${AUTHOR}/g" \
-	| sed "s/@@LICENSE@@/${LICENSE}/g" \
-	| sed "s/@@VERSION@@/${VERSION}/g" \
-	| sed "s/@@BUILD_DATE@@/${BUILD_DATE}/g" \
-	| sed "s/@@FILE_NAME@@/${FILE%%.tmpl}/g" #> ${FILE%%.tmpl}
+		| sed "s/@@APP_NAME@@/${APP_NAME}/g" \
+		| sed "s/@@AUTHOR@@/${AUTHOR}/g" \
+		| sed "s/@@LICENSE@@/${LICENSE}/g" \
+		| sed "s/@@VERSION@@/${VERSION}/g" \
+		| sed "s/@@BUILD_DATE@@/${BUILD_DATE}/g" \
+		| sed "s/@@FILE_NAME@@/${FILE%%.tmpl}/g" > ${FILE%%.tmpl}
 }
 
 PRINT_INFO "$FILE -> Running... @ $DATE"
@@ -82,23 +82,25 @@ pushd ./src > /dev/null
 if [[ ${LICENSE:="None"} = "GPL" || ${LICENSE:="None"} = "BSD" ]]; then
 	cat ~/bin/${LICENSE}_header.snip ./@@APP_NAME@@.cpp.tmpl > ./@@APP_NAME@@.cpp.tmpl.tmp
 	mv ./@@APP_NAME@@.cpp.tmpl.tmp ./@@APP_NAME@@.cpp.tmpl
-	# cat ~/bin/${LICENSE}_header.snip ./@@APP_NAME@@.hpp.tmpl > ./@@APP_NAME@@.hpp.tmpl.tmp
-	# mv ./@@APP_NAME@@.hpp.tmpl.tmp ./@@APP_NAME@@.hpp.tmpl
+	cat ~/bin/${LICENSE}_header.snip ./@@APP_NAME@@.hpp.tmpl > ./@@APP_NAME@@.hpp.tmpl.tmp
+	mv ./@@APP_NAME@@.hpp.tmpl.tmp ./@@APP_NAME@@.hpp.tmpl
 	cat ~/bin/${LICENSE}_header.snip ./main.cpp.tmpl > ./main.cpp.tmpl.tmp
 	mv ./main.cpp.tmpl.tmp ./main.cpp.tmpl
-	cat ~/bin/${LICENSE}_header.snip ./main.hpp.tmpl > ./main.hpp.tmpl.tmp
-	mv ./main.hpp.tmpl.tmp ./main.hpp.tmpl
+	# cat ~/bin/${LICENSE}_header.snip ./main.hpp.tmpl > ./main.hpp.tmpl.tmp
+	# mv ./main.hpp.tmpl.tmp ./main.hpp.tmpl
 fi
 
 #ADD_HEADERS ./main.cpp.tmpl
 
-cat  ./main.cpp.tmpl \
+FILE_TEST="main.cpp.tmpl"
+F=${FILE_TEST%%.tmpl}
+cat  "./main.cpp.tmpl" \
 	| sed "s/@@APP_NAME@@/${APP_NAME}/g" \
 	| sed "s/@@AUTHOR@@/${AUTHOR}/g" \
 	| sed "s/@@LICENSE@@/${LICENSE}/g" \
 	| sed "s/@@VERSION@@/${VERSION}/g" \
 	| sed "s/@@BUILD_DATE@@/${BUILD_DATE}/g" \
-	| sed "s/@@FILE_NAME@@/main.cpp/g" > main.cpp
+	| sed "s/@@FILE_NAME@@/${FILE_TEST%%tmpl}/g" > ./main.cpp
 
 popd > /dev/null
 
