918b044258a7c28a268485f23efe0f1526e7bd29
Brian K Preston <brian8181@gmail.com>
2023-08-29 20:01:51 -0500

    0000093: ccl not working~testing

--- test/class_names.sh
+++ test/class_names.sh
@@ -13,3 +13,7 @@ BASE_CLASS_NAME=${INPUT##*::}
 
 echo "CLASS_NAME=${CLASS_NAME}"
 echo "BASE_CLASS_NAME=${BASE_CLASS_NAME}"
+
+if [ $BASE_CLASS_NAME = $CLASS_NAME ]; then 
+    echo "no base ..."
+fi
