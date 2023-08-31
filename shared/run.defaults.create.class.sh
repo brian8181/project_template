a01df2ce079c5ca875433f8b643ab476b0d2f4a6
Brian K Preston <brian8181@gmail.com>
2023-08-31 16:59:24 -0500

    fixing defaults ...

--- shared/run.defaults.create.class.sh
+++ shared/run.defaults.create.class.sh
@@ -1,8 +1,7 @@
 CLASS_NAME=$1
 LICENSE="None"
 AUTHOR="Brian K Preston"
-FILE_NAME=$2
 BUILD_DATE=$(date)
 VERSION="0.0.1"
 
-~/bin/create.class.sh ${CLASS_NAME} "." "." ${LICENSE} "${AUTHOR}" "${BUILD_DATE}" ${VERSION} $FILE_NAME
+~/bin/create.class.sh ${CLASS_NAME} ${LICENSE} "${AUTHOR}" "${BUILD_DATE}" ${VERSION}
