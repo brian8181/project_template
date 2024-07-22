6400af32febb12c81c22644ace473bd3c92aa448
Brian K <brian8181@gmail.com>
2024-07-22 11:46:32 -0500

    add build & remove info from header ...'

--- shared/csk.sh
+++ shared/csk.sh
@@ -2,9 +2,6 @@
 
 FILE='./shared/csk.sh'
 FILE_DATE=$(date)
-AUTHOR='Brian K Preston'
-EMAIL='brian8181@gmail.com'
-WWW='https://github.com/brian8181'
 BUILD_DATE=$(date)
 VERSION="3.6.7"
 USER_NAME=$(whoami)
