f88037a0cdca3a22dda1f4ac6a2c77ecec409ce0
Brian K Preston <brian8181@gmail.com>
2024-10-04 10:52:38 -0500

    update ....

--- install.sh
+++ install.sh
@@ -6,14 +6,8 @@
 # Version:    0.1.0
 
 FILE='./install.sh'
-<<<<<<< HEAD
 VERSION='0.1.1'
 FILE_DATE='Thu Aug 15 10:17:45 PM CDT 2024'
-=======
-VERSION='0.1.7'
-FILE_DATE='Tue Sep 12 09:50:26 AM CDT 2023'
-FILE_DATE='Tue Nov 14 03:34:38 AM CDT 2023'
->>>>>>> 2ee88bdf43d57d20741313181bba6426831c6778
 
 FMT_FG_RED='\e[31m'
 FMT_FG_GREEN='\e[32m'
@@ -46,17 +40,13 @@ function PRINT_INFO
 }
 
 PRINT_INFO "$FILE -> Running... @ $DATE"
-<<<<<<< HEAD
 ##{ BEGIN CODE  }##
 
-=======
->>>>>>> 2ee88bdf43d57d20741313181bba6426831c6778
 PRINT_INFO "remove existing project templates ..."
 # remove "templates" before update
 rm -rf ~/bin/templates
 PRINT_INFO "install project templates ..."
 # copy templates
-<<<<<<< HEAD
 mkdir ~/bin/project_templates
 cp -r ./basic ~/bin/project_templates
 cp -r ./minimal ~/bin/project_templates
@@ -80,13 +70,11 @@ chmod +x ~/bin/skel.sh
 
 # create easy name soft link
 ln -s ~/bin/csk.sh ~/bin/csk
-=======
 mkdir ~/bin/templates
 cp -r ./templates/basic ~/bin/templates
 cp -r ./templates/basic_nam ~/bin/templates
 cp -r ./templates/minimal ~/bin/templates
 cp -r ./templates/gtk ~/bin/templates
->>>>>>> 2ee88bdf43d57d20741313181bba6426831c6778
 
 PRINT_INFO "remove existing class templates ..."
 PRINT_INFO "copy shared files to mbin ..."
@@ -107,12 +95,7 @@ ln -s ~/bin/csk.sh ~/bin/csk
 ln -s ~/bin/csk-a.sh ~/bin/csk-a
 ln -s ~/bin/run.defaults.create.class.sh ~/bin/ccl
 # create new link with multi name option built into script
-<<<<<<< HEAD
 ln -s ~/bin/class_tmpl/run.create.class.sh ~/bin/ccl2
 
 ##{ END CODE  }##
-=======
-ln -s ~/bin/run.create.class.sh ~/bin/ccl2
-
->>>>>>> 2ee88bdf43d57d20741313181bba6426831c6778
 PRINT_INFO "$FILE -> Exiting.   @ $DATE"
--- shared/run.defaults.create.class.sh
+++ shared/run.defaults.create.class.sh
@@ -1,7 +1,5 @@
 CLASS_NAME=$1
-LICENSE="None"
-AUTHOR="Brian K Preston"
 BUILD_DATE=$(date)
 VERSION="0.0.1"
 
-~/bin/create.class.sh ${CLASS_NAME} ${LICENSE} "${AUTHOR}" "${BUILD_DATE}" ${VERSION}
+~/bin/create.class.sh ${CLASS_NAME} "" "" "" "0.0.1"
