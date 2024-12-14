865362ce95daa55050505eb27ded29cefc83a150
Brian K Preston <brian8181@gmail.com>
2024-12-14 08:46:03 -0600

    add new basic tmpl, combine main with app.cpp ...

---
+++ templates/basic/src/tmpl.cpp
@@ -0,0 +1,116 @@
+// std c
+#include <stdlib.h>
+#include <fmt/color.h>
+#include <unistd.h>         /* for STDIN_FILENO */
+#include <sys/select.h>     /* for pselect   */
+#include <getopt.h>
+// std cpp
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+// local 
+#include "bash_color.h"
+
+
+using std::string;
+using std::stringstream;
+using std::vector;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+const string VERSION_STRING = "0.0.1";
+const int DEFAULT_ARGC = 2;
+const unsigned short VERBOSE          = 0x01;
+const unsigned short DEFAULTS         = 0x00;
+const unsigned short FIELDS           = 0x02;
+unsigned short options = DEFAULTS;
+char DELIMITER = ',';
+
+static struct option long_options[] =
+{
+        {"verbose", no_argument, 0, 'v'},
+        {"help", no_argument, 0, 'h'},
+        {"version", no_argument, 0, 'r'},
+};
+
+unsigned short OPTION_FLAGS = DEFAULTS;
+
+void print_version()
+{
+	cout << VERSION_STRING << endl;
+}
+
+void print_help()
+{
+	cout	<< endl 
+			<< FMT_BOLD << FMT_FG_GREEN << "Usage: "            << FMT_RESET << endl
+			<< FMT_BOLD << FMT_FG_BLUE  << "split"              << FMT_RESET << " "
+			<< FMT_FG_BLUE              << "[-hvr][...]"        << FMT_RESET << " "
+			                            << "FILE [FILE2 ... ]" << FMT_RESET << " "
+																<<  endl << endl;
+}
+
+int parse_options(int argc, char* argv[])
+{
+	int opt = 0;
+	int option_index = 0;
+	optind = 0;
+	while ((opt = getopt_long(argc, argv, "hv ", long_options, &option_index)) != -1)
+	{
+		switch (opt)
+		{
+			case 'h':
+				print_help();
+			case 'v':
+				print_version();
+				return 0;
+		}
+	}
+
+	if (argc < DEFAULT_ARGC) // not correct number of args
+	{
+		cerr << "Expected argument after options, -h for help" << endl;
+		return -1;
+	}
+
+	string path = argv[optind]; // get file path
+
+	return 0;
+}
+
+int stdin_ready (int filedes)
+{
+	fd_set set;
+	// declare/initialize zero timeout 
+	struct timespec timeout = { .tv_sec = 0 };
+	// initialize the file descriptor set
+	FD_ZERO(&set);
+	FD_SET(filedes, &set);
+	// check stdin_ready is ready on filedes 
+	return pselect(filedes + 1, &set, NULL, NULL, &timeout, NULL);
+}
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		if(stdin_ready(STDIN_FILENO))
+		{
+			std::string buffer;
+			std::cin >> buffer;
+			// add piped buffer to end of argv
+			char* argvtmp[sizeof(char*) * argc+1];
+			memcpy(argvtmp, argv, sizeof(char*) * argc);
+			argvtmp[argc] = &buffer[0];
+			argv = argvtmp;
+			++argc;
+		}
+		return parse_options(argc, argv);
+	}
+	catch(std::logic_error& ex)
+	{
+	 	std::cout << ex.what() << std::endl;
+	}
+}
