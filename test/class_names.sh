016010a73fbe0b8c89f52a482c3412c63a500c84
Brian K Preston <brian8181@gmail.com>
2024-12-23 12:52:14 -0600

    clean up test dir ...

--- test/class_names.sh
+++ test/class_names.sh
@@ -5,11 +5,8 @@
 # square::shape
 
 INPUT=$1
-echo ${INPUT%%::*}
-echo ${INPUT##*::}
-
-CLASS_NAME=${INPUT%%::*}
-BASE_CLASS_NAME=${INPUT##*::}
+CLASS_NAME=${INPUT%%:*}
+BASE_CLASS_NAME=${INPUT##*:}
 
 echo "CLASS_NAME=${CLASS_NAME}"
 echo "BASE_CLASS_NAME=${BASE_CLASS_NAME}"
@@ -18,6 +15,3 @@ if [ $BASE_CLASS_NAME = $CLASS_NAME ]; then
     echo "no base ..."
 fi
 
-if [ $BASE_CLASS_NAME = "shape1" ]; then 
-    echo "testing ..."
-fi
