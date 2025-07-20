be61c5fb7503704eea3ba9cd7717e8483633fc63
Brian K Preston <brian8181@gmail.com>
2025-07-20 16:25:03 -0500

    update all ...

--- templates/class/less_than_equal_overload.php
+++ templates/class/less_than_equal_overload.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 bool <?php echo $CLASSNAME ?>::operator<( const <?php echo $CLASSNAME ?>& that)
 {
     return false;
 }
-=======
-<php? # lessthan_equal operator overload  ?>
->>>>>>> a49a0a10ef28fff82fb177765bc0093e17e8fcaf
