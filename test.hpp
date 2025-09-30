f5f33943eccd5d93302357c20bd22d1776b3d4f6
Brian K Preston <brian8181@gmail.com>
2025-09-30 10:06:16 -0500

    auto commit, add & commit all ...

---
+++ test.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file    test.hpp
+ * @version 0.0.1
+ * @date    Tue, 30 Sep 2025 14:51:08 +0000
+ * @info    ...
+ */
+#ifndef _test_HPP_
+#define _test_HPP_
+#include <iostream>
+
+/**
+  * @brief class test
+  */
+class test
+{
+public:
+	/**
+     * @brief : default ctor
+     */
+	test();
+
+	/**
+     * @brief : copy ctor
+     */
+	test( const test& src );
+
+  	/**
+  	 * @brief : destructor
+  	 */
+	virtual ~test();
+
+	/**
+	  * @brief 
+	  * @brief c++ comment ...
+	  * @brief place future addtions here ...
+	  *
+	*/
+
+private:
+
+};
+
+#endif
